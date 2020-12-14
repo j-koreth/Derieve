aab1f54b22cb98a2abba29f61a00409a1d5e02a6
Joel Koreth <joelsymon@gmail.com>
2020-12-14 11:49:33 -0600

    Test

--- app.py
+++ app.py
@@ -141,5 +141,6 @@ def fifth():
 
     return render_template('/fifth.html',**locals())
 
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
